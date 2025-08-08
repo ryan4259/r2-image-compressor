06cfbca08e5f161ac1e84f1d864ec15b05f72179
ryan4259 <90567196+ryan4259@users.noreply.github.com>
2025-08-08 16:47:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const upload = multer();
 
 const s3 = new S3Client({
   region: 'auto',
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: process.env.R2_ENDPOINT, // <--- changed here to use env var directly
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
   },
-  forcePathStyle: true, // <--- This is required for Cloudflare R2
+  forcePathStyle: true, // required for Cloudflare R2
 });
 
 app.post('/', upload.single('file'), async (req, res) => {
